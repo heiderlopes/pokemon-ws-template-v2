bb5e2f2a64e548322fe0d5617a339e18a6d14f53
Heider Lopes <heidertreinamentos@gmail.com>
2021-08-19 18:38:45 -0300

    Ajustes nos nomes

--- app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/presentation/formpokemon/FormPokemonActivity.kt
+++ app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/presentation/formpokemon/FormPokemonActivity.kt
@@ -1,12 +1,12 @@
-package br.com.heiderlopes.pokemonwstemplatev2.presentation.form
+package br.com.heiderlopes.pokemonwstemplatev2.presentation.formpokemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.heiderlopes.pokemonwstemplatev2.R
 
-class FormActivity : AppCompatActivity() {
+class FormPokemonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form)
+        setContentView(R.layout.activity_form_pokemon)
     }
 }
